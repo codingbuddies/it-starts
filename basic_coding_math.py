2c97b2f6a86725f44a7c65f3161e88e8fa2d3c2d
Victoria Payne <vhpayne@gmail.com>
2015-07-18 18:03:28 -0700

    Practice files for Nathan

---
+++ basic_coding_math.py
@@ -0,0 +1,27 @@
+def add(num1, num2):
+	#This is an example.  Just basic arithmetic.
+	#Go through the spreadsheet.  Make basic functions that do what you want them to do.
+	#Replace pass with what you want.
+    return num1 + num2
+
+def subtract(num1, num2):
+	pass
+
+def multiply(num1, num2):
+    pass
+
+def divide(num1, num2):
+    pass
+
+def square(num1):
+    pass
+
+def cube(num1):
+    pass
+
+def power(num1, num2):
+	pass
+
+def mod(num1, num2):
+	# You might need to google modulo.  I believe in you!
+    pass
